dbfe56a7483a95d0585db39a6766d7493a9e88bc
Stephen Parsons <srpa226@uky.edu>
2021-04-09 10:09:06 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import *
+from tensorflow import keras
+from tensorflow.keras import layers
 import bnn_util
 import json
 
@@ -9,11 +9,10 @@ def restore_model(run):
     # load opts used during training
     opts = json.loads(open("ckpts/%s/opts.json" % run).read())
 
-    # NOTE: we construct this model with full width / height, not the (potential)
-    #       patch size we used for training
+    # NOTE: we construct this model with unspecified width/height so we can pass in anything
     model = construct_model(
-        width=opts['width'],
-        height=opts['height'],
+        width=None,
+        height=None,
         use_skip_connections=not opts['no_use_skip_connections'],
         base_filter_size=opts['base_filter_size'],
         use_batch_norm=not opts['no_use_batch_norm']
@@ -37,15 +36,15 @@ def construct_model(width, height, base_filter_size,
         #    if use_batch_norm:
         #      o = BatchNormalization(scale=False)(o)
 
-        o = Conv2D(filters=filters, kernel_size=3,
-                   strides=strides, padding='same')(i)
+        o = layers.Conv2D(filters=filters, kernel_size=3,
+                          strides=strides, padding='same')(i)
         if use_batch_norm:
-            o = BatchNormalization()(o)
+            o = layers.BatchNormalization()(o)
 
         # TODO: try BN after relu
-        return ReLU()(o)
+        return layers.ReLU()(o)
 
-    inputs = Input(shape=(height, width, 3), name='inputs')
+    inputs = layers.Input(shape=(height, width, 3), name='inputs')
 
     e1 = conv_bn_relu_block(inputs, 'e1', filters=base_filter_size, strides=2)
     e2 = conv_bn_relu_block(e1, 'e2', filters=2 * base_filter_size, strides=2)
@@ -55,25 +54,25 @@ def construct_model(width, height, base_filter_size,
     # note: using version of keras locally that doesn't support interpolation='nearest' so
     #       unsure what resize is happening here...
 
-    d1 = UpSampling2D(name='e4nn')(e4)
+    d1 = layers.UpSampling2D(name='e4nn')(e4)
     if use_skip_connections:
-        d1 = Concatenate(name='d1_e3')([d1, e3])
+        d1 = layers.Concatenate(name='d1_e3')([d1, e3])
     d1 = conv_bn_relu_block(d1, 'd1', filters=4 * base_filter_size, strides=1)
 
-    d2 = UpSampling2D(name='d1nn')(d1)
+    d2 = layers.UpSampling2D(name='d1nn')(d1)
     if use_skip_connections:
-        d2 = Concatenate(name='d2_e2')([d2, e2])
+        d2 = layers.Concatenate(name='d2_e2')([d2, e2])
     d2 = conv_bn_relu_block(d2, 'd2', filters=2 * base_filter_size, strides=1)
 
-    d3 = UpSampling2D(name='d2nn')(d2)
+    d3 = layers.UpSampling2D(name='d2nn')(d2)
     if use_skip_connections:
-        d3 = Concatenate(name='d3_e1')([d3, e1])
+        d3 = layers.Concatenate(name='d3_e1')([d3, e1])
     d3 = conv_bn_relu_block(d3, 'd3', filters=base_filter_size, strides=1)
 
-    logits = Conv2D(filters=1, kernel_size=1, strides=1,
-                    activation=None, name='logits')(d3)
+    logits = layers.Conv2D(filters=1, kernel_size=1, strides=1,
+                           activation=None, name='logits')(d3)
 
-    return Model(inputs=inputs, outputs=logits)
+    return keras.Model(inputs=inputs, outputs=logits)
 
 
 def compile_model(model, learning_rate, pos_weight=1.0):
